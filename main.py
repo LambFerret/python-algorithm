28adf846f594061ee6e17eb0d6f6a1b21cf3fda6
LambFerret <dignrjrl12@gmail.com>
2022-06-17 16:13:46 +0900

    2231 분해합

--- main.py
+++ main.py
@@ -8,41 +8,39 @@ n, m, k, arr = 0, 0, 0, []
 
 
 def answer(x, y, input_list):
-    ans_array = []
-    for i in range(len(input_list)):
-        for j in range(i+1, len(input_list)):
-            for l in range(j+1, len(input_list)):
-                ans_array.append(input_list[i]+input_list[j]+input_list[l])
+    ans = []
+    for i in range(x):
+        process = sum(list(map(int, list(str(i)))))
+        # print(x,i, process, i+process,  x == i+process)
+        if x == i+process:
+            return i
+    return 0
 
-    result = 0
-    for i in sorted(ans_array):
-        # print(result, ans_array[i])
-        if i <= y:
-            result = i
-    print(result)
+    # print(x, y, input_list)
 
 
 """N, M Input"""
 # 숫자 n을 받은 경우
-# n = int(sys.stdin.readline())
+n = int(sys.stdin.readline())
 # m = int(sys.stdin.readline())
 
 # 숫자 n, m을 받은 경우
-n, m = map(int, sys.stdin.readline().split())
+# n, m = map(int, sys.stdin.readline().split())
 
 """List Input"""
 # string을 n줄 받는 경우
-# for _ in range(n):
-#     arr.append(sys.stdin.readline().replace('\n', ''))
+for _ in range(n):
+    arr.append(sys.stdin.readline().split())
 
 
 # int를 한줄에 받은 경우
-arr = list(map(int,sys.stdin.readline().split()))
+# arr = list(map(int,sys.stdin.readline().split()))
 
 # int를 n줄 받은 경우
 # for _ in range(n):
 #     arr.append(int(sys.stdin.readline().replace('\n', '')))
 
 
-answer(n, m, arr)
+a = answer(n, m, arr)
+print(a)
 
