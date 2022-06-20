b8a41bdcde28571a7034d8e5e2f81539196ab2b1
LambFerret <dignrjrl12@gmail.com>
2022-06-20 11:01:12 +0900

    1436 영화감독 숌

--- main.py
+++ main.py
@@ -8,39 +8,30 @@ n, m, k, arr = 0, 0, 0, []
 
 
 def answer(x, y, input_list):
-    least_array = []
-    who_is_first = ['WBWBWBWB', 'BWBWBWBW']
-    first = ''
-    for c in range(2):
-        first = True if c else False
-        for i in range(x - 7):
-            for j in range(y - 7):
-                count = 0
-                for a in range(8):
-                    for b in range(8):
-                        compare = who_is_first[0] if first else who_is_first[1]
-                        if compare[b] != input_list[i + a][j + b]:
-                            count += 1
-                    first = not first
-                least_array.append(count)
-    return int(min(least_array))
-    return (x, y, input_list)
+    count = 0
+    ans = 0
+    while True:
+        ans += 1
+        if '666' in str(ans):
+            count += 1
+        if count == x:
+            return ans
 
-    # print(x, y, input_list)
+    return (x, y, input_list)
 
 
 """N, M Input"""
 # 숫자 n을 받은 경우
-# n = int(sys.stdin.readline())
+n = int(sys.stdin.readline())
 # m = int(sys.stdin.readline())
 
 # 숫자 n, m을 받은 경우
-n, m = map(int, sys.stdin.readline().split())
+# n, m = map(int, sys.stdin.readline().split())
 
 """List Input"""
 # string을 n줄 받는 경우
-for _ in range(n):
-    arr.append(sys.stdin.readline().replace('\n', ''))
+# for _ in range(n):
+#     arr.append(sys.stdin.readline().replace('\n', ''))
 
 # int를 한줄에 받은 경우
 # arr = list(map(int,sys.stdin.readline().split()))
