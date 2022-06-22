c6b9dbdc920afb02ecf26225def7712ad925f87c
LambFerret <dignrjrl12@gmail.com>
2022-06-22 15:20:49 +0900

    1181 단어 정렬

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import sys
-import math
-from collections import Counter
 
 # init
+
 n, m, k, arr = 0, 0, 0, []
 
 
@@ -10,13 +9,15 @@ n, m, k, arr = 0, 0, 0, []
 
 
 def answer(x, y, input_list):
-    input_list.sort()
-
+    input_list = list(set(input_list))
+    len_list = []
     for i in input_list:
-        print(i[1],i[0])
-
-    return (x, y, input_list)
-
+        len_list.append([len(i), i])
+    len_list.sort()
+    for i in len_list:
+        print(i[1])
+    return len_list
+    return x, y, input_list
 
 """N, M Input"""
 # 숫자 n을 받은 경우
@@ -29,9 +30,8 @@ n = int(sys.stdin.readline())
 """List Input"""
 # string을 n줄 받는 경우
 for _ in range(n):
-    list_a = sys.stdin.readline().replace('\n', '').split()
-    list_a.reverse()
-    arr.append(tuple(map(int,list_a)))
+    arr.append(sys.stdin.readline().replace('\n', ''))
+
 
 # int를 한줄에 받은 경우
 # arr = list(map(int,sys.stdin.readline().split()))
@@ -40,5 +40,7 @@ for _ in range(n):
 # for _ in range(n):
 #     arr.append(int(sys.stdin.readline().replace('\n', '')))
 
-answer(n,m,arr)
-# print(answer(n, m, arr))
+
+result = answer(n, m, arr)
+
+# print(result)
