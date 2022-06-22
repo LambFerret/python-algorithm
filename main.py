7c12efd3a792e8b11262124ea503e4faf7728b39
LambFerret <dignrjrl12@gmail.com>
2022-06-22 17:15:24 +0900

    1085 직사각형에서 탈출

--- main.py
+++ main.py
@@ -8,21 +8,18 @@ n, m, k, arr = 0, 0, 0, []
 # constant
 
 
-def answer(x, y, input_list):
-    sorted_list = sorted(list(set(input_list)))
-    a = {sorted_list[i]: i for i in range(len(sorted_list))}
-    result1 = [a[i] for i in input_list]
-    return result1
+def answer(x, y, w, h, input_list):
+    return min([x, y, w - x, h - y])
     return x, y, input_list
 
 
 """N, M Input"""
 # 숫자 n을 받은 경우
-n = int(sys.stdin.readline())
+# n = int(sys.stdin.readline())
 # m = int(sys.stdin.readline())
 
 # 숫자 n, m을 받은 경우
-# n, m = map(int, sys.stdin.readline().split())
+n, m, w, h = map(int, sys.stdin.readline().split())
 
 """List Input"""
 # string을 n줄 받는 경우
@@ -31,14 +28,13 @@ n = int(sys.stdin.readline())
 
 
 # int를 한줄에 받은 경우
-arr = list(map(int, sys.stdin.readline().split()))
+# arr = list(map(int, sys.stdin.readline().split()))
 
 # int를 n줄 받은 경우
 # for _ in range(n):
 #     arr.append(int(sys.stdin.readline().replace('\n', '')))
 
 
-result = answer(n, m, arr)
+result = answer(n, m, w, h, arr)
 
-for a in result:
-    print(a)
+print(result)
