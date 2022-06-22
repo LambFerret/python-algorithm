aae3d77f8245d93bb39c7221b5ee35d77af0c7e9
LambFerret <dignrjrl12@gmail.com>
2022-06-22 15:12:05 +0900

    11651 좌표 정렬하기 2

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ def answer(x, y, input_list):
     input_list.sort()
 
     for i in input_list:
-        print(i[0],i[1])
+        print(i[1],i[0])
 
     return (x, y, input_list)
 
@@ -29,7 +29,9 @@ n = int(sys.stdin.readline())
 """List Input"""
 # string을 n줄 받는 경우
 for _ in range(n):
-    arr.append(tuple(map(int,sys.stdin.readline().replace('\n', '').split())))
+    list_a = sys.stdin.readline().replace('\n', '').split()
+    list_a.reverse()
+    arr.append(tuple(map(int,list_a)))
 
 # int를 한줄에 받은 경우
 # arr = list(map(int,sys.stdin.readline().split()))
