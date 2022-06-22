43e0af924f77d361616ebabdb9c4d8308290a6c4
LambFerret <dignrjrl12@gmail.com>
2022-06-22 17:28:48 +0900

    3009 네 번째 점

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
 import sys
-
+from collections import Counter
 # init
 
 n, m, k, arr = 0, 0, 0, []
+arr2 = []
 
 
 # constant
 
 
-def answer(x, y, w, h, input_list):
-    return min([x, y, w - x, h - y])
+def answer(x, y, input_list):
     return x, y, input_list
 
+def answer2(ls1, ls2):
+    print(Counter(ls1).most_common()[1][0])
+    print(Counter(ls2).most_common()[1][0])
+
+    # return c[1],d[1]
 
 """N, M Input"""
 # 숫자 n을 받은 경우
@@ -19,7 +24,7 @@ def answer(x, y, w, h, input_list):
 # m = int(sys.stdin.readline())
 
 # 숫자 n, m을 받은 경우
-n, m, w, h = map(int, sys.stdin.readline().split())
+# n, m = map(int, sys.stdin.readline().split())
 
 """List Input"""
 # string을 n줄 받는 경우
@@ -31,10 +36,13 @@ n, m, w, h = map(int, sys.stdin.readline().split())
 # arr = list(map(int, sys.stdin.readline().split()))
 
 # int를 n줄 받은 경우
-# for _ in range(n):
-#     arr.append(int(sys.stdin.readline().replace('\n', '')))
+for _ in range(3):
+    a, b = list(map(int, (sys.stdin.readline().split())))
+    arr.append(a)
+    arr2.append(b)
 
+# result = answer(n, m, arr)
+# print(result)
 
-result = answer(n, m, w, h, arr)
-
-print(result)
+result = answer2(arr, arr2)
+# print(result)
