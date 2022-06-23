b0c393c68aed3e40acfd468b552251f23528d677
LambFerret <dignrjrl12@gmail.com>
2022-06-23 10:29:02 +0900

    4153 직각삼각형

--- main.py
+++ main.py
@@ -1,22 +1,22 @@
 import sys
-from collections import Counter
+
 # init
 
 n, m, k, arr = 0, 0, 0, []
-arr2 = []
 
 
 # constant
 
 
 def answer(x, y, input_list):
+    for i in input_list:
+        i.sort()
+        if i[0]**2 + i[1]**2 == i[2]**2:
+            print('right')
+        else:
+            print('wrong')
     return x, y, input_list
 
-def answer2(ls1, ls2):
-    print(Counter(ls1).most_common()[1][0])
-    print(Counter(ls2).most_common()[1][0])
-
-    # return c[1],d[1]
 
 """N, M Input"""
 # 숫자 n을 받은 경우
@@ -33,16 +33,19 @@ def answer2(ls1, ls2):
 
 
 # int를 한줄에 받은 경우
-# arr = list(map(int, sys.stdin.readline().split()))
+# arr = list(map(int,sys.stdin.readline().split()))
 
 # int를 n줄 받은 경우
-for _ in range(3):
-    a, b = list(map(int, (sys.stdin.readline().split())))
-    arr.append(a)
-    arr2.append(b)
+# for _ in range(n):
+#     arr.append(int(sys.stdin.readline().replace('\n', '')))
 
-# result = answer(n, m, arr)
-# print(result)
+# int를 특정 문자열 까지 받는 경우
+while True:
+    data = list(map(int, sys.stdin.readline().split()))
+    if data == [0, 0, 0]:
+        break
+    arr.append(data)
 
-result = answer2(arr, arr2)
+result = answer(n, m, arr)
+
 # print(result)
