5b0040ac74c7ecf6a7b776bcab078b08f27d2a20
LambFerret <dignrjrl12@gmail.com>
2022-06-23 13:45:52 +0900

    init

--- main.py
+++ main.py
@@ -7,14 +7,7 @@ n, m, k, arr = 0, 0, 0, []
 
 # constant
 
-
 def answer(x, y, input_list):
-    for i in input_list:
-        i.sort()
-        if i[0]**2 + i[1]**2 == i[2]**2:
-            print('right')
-        else:
-            print('wrong')
     return x, y, input_list
 
 
@@ -24,7 +17,7 @@ def answer(x, y, input_list):
 # m = int(sys.stdin.readline())
 
 # 숫자 n, m을 받은 경우
-# n, m = map(int, sys.stdin.readline().split())
+n, m = map(int, sys.stdin.readline().split())
 
 """List Input"""
 # string을 n줄 받는 경우
@@ -39,13 +32,17 @@ def answer(x, y, input_list):
 # for _ in range(n):
 #     arr.append(int(sys.stdin.readline().replace('\n', '')))
 
+# list<int>를 n줄 받은 경우
+# for _ in range(n):
+#     arr.append(list(map(int,sys.stdin.readline().split())))
+
 # int를 특정 문자열 까지 받는 경우
-while True:
-    data = list(map(int, sys.stdin.readline().split()))
-    if data == [0, 0, 0]:
-        break
-    arr.append(data)
+# while True:
+#     data = list(map(int, sys.stdin.readline().split()))
+#     if data == [0, 0, 0]:
+#         break
+#     arr.append(data)
 
 result = answer(n, m, arr)
 
-# print(result)
+print(result)
