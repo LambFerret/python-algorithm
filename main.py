5a5ff5d7a3545aea93186a21ad60e1b2748ccfd6
LambFerret <dignrjrl12@gmail.com>
2022-06-28 18:13:44 +0900

    1874 스택 수열

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import sys
-import re
 
 # init
 
@@ -7,39 +6,53 @@ n, m, k, arr = 0, 0, 0, []
 
 
 # constant
+class Stack:
+    def __init__(self):
+        self.stack = []
+
+    def push(self, x):
+        self.stack.append(x)
+        return ('+')
+
+    def pop(self):
+        return self.stack.pop(-1)
+
+        # return a
+
 
 def answer(x, y, input_list):
     ans = []
-    for a in input_list:
-        print(f(a))
-
+    s = Stack()
+    is_correct = True
+    i = 1
+    while input_list:
+        if i <= input_list[0]:
+            ans.append(s.push(i))
+            i += 1
+            # print('아직 작음 ㄱ', input_list)
+            # print('아직 작음 ㄴ', i)
+            # print('아직 작음 ㄷ', s.stack)
+            continue
+        if i > input_list[0]:
+            # print('정답 ㄱ', input_list)
+            # print('정답 ㄴ', i)
+            # print('정답 ㄷ', s.stack)
+            spop = s.pop()
+            inputpop = input_list.pop(0)
+            if spop != inputpop:
+                is_correct = False
+            ans.append(spop)
+    if is_correct:
+        for al in ans:
+            print('+' if al == '+' else '-')
+    else:
+        print('NO')
     return x, y, input_list
-
-
-def f(string):
-    string = re.sub('[^()[\\]]', '', string)
-    temp = []
-    try:
-        for b in string:
-            if b == '(':
-                temp.append(b)
-            elif b == ')':
-                if temp.pop(-1) != '(':
-                    return 'no'
-            elif b == '[':
-                temp.append(b)
-            elif b == ']':
-                if temp.pop(-1) != '[':
-                    return 'no'
-
-        return 'yes' if len(temp) == 0 else 'no'
-    except IndexError:
-        return 'no'
 
 
 """N, M Input"""
 # 숫자 n을 받은 경우
-# n = int(sys.stdin.readline())
+n = int(sys.stdin.readline())
 # m = int(sys.stdin.readline())
 
 # 숫자 n, m을 받은 경우
@@ -55,8 +68,8 @@ def f(string):
 # arr = list(map(int,sys.stdin.readline().split()))
 
 # int를 n줄 받은 경우
-# for _ in range(n):
-#     arr.append(int(sys.stdin.readline().replace('\n', '')))
+for _ in range(n):
+    arr.append(int(sys.stdin.readline().replace('\n', '')))
 
 # list<int>를 n줄 받은 경우
 # for _ in range(n):
@@ -74,11 +87,11 @@ def f(string):
 #     arr.append(data)
 
 # str를 특정 문자열 까지 받는 경우
-while True:
-    data = sys.stdin.readline().replace('\n', '')
-    if data == '.':
-        break
-    arr.append(data)
+# while True:
+#     data = sys.stdin.readline().replace('\n', '')
+#     if data == '.':
+#         break
+#     arr.append(data)
 
 result = answer(n, m, arr)
 
