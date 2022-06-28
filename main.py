ba7170834bd6acd01a47d9bcad3c2415d9150c9f
LambFerret <dignrjrl12@gmail.com>
2022-06-28 14:25:05 +0900

    10828 스택

--- main.py
+++ main.py
@@ -7,12 +7,47 @@ n, m, k, arr = 0, 0, 0, []
 
 
 # constant
-PIE = math.pi
+class Stack:
+    def __init__(self):
+        self.stack = []
+
+    def push(self, x):
+        self.stack.append(x)
+
+    def pop(self):
+        try:
+            return self.stack.pop(-1)
+        except IndexError:
+            return -1
+
+    def size(self):
+        return len(self.stack)
+
+    def is_empty(self):
+        return 0 if len(self.stack)>0 else 1
+
+    def top(self):
+        try:
+            return self.stack[-1]
+        except IndexError:
+            return -1
+
+
 def answer(x, y, input_list):
-    a = x**2 * PIE
-    b = x**2 * 2
-    return "{:.06f}".format(a), "{:.06f}".format(b)
-    return x, y, input_list
+    s = Stack()
+    for i in input_list:
+        if i[0] == 'push':
+            s.push(i[1])
+        elif i[0] == 'pop':
+            print(s.pop())
+        elif i[0] == 'size':
+            print(s.size())
+        elif i[0] == 'empty':
+            print(s.is_empty())
+        elif i[0] == 'top':
+            print(s.top())
+
+    # return x, y, input_list
 
 
 """N, M Input"""
@@ -40,6 +75,10 @@ n = int(sys.stdin.readline())
 # for _ in range(n):
 #     arr.append(list(map(int,sys.stdin.readline().split())))
 
+# list<str>를 n줄 받은 경우
+for _ in range(n):
+    arr.append(list(map(str, sys.stdin.readline().split())))
+
 # int를 특정 문자열 까지 받는 경우
 # while True:
 #     data = list(map(int, sys.stdin.readline().split()))
@@ -48,5 +87,5 @@ n = int(sys.stdin.readline())
 #     arr.append(data)
 
 result = answer(n, m, arr)
-for a in result:
-    print(a)
+# for a in result:
+#     print(a)
