94e895c9169fe23f185150ade04b949478458b88
LambFerret <dignrjrl12@gmail.com>
2022-06-30 15:51:51 +0900

    1966 프린터 큐

--- main.py
+++ main.py
@@ -8,23 +8,41 @@ n, m, k, arr = 0, 0, 0, []
 # constant
 
 def answer(x, y, input_list):
-    q = deque(range(1, x + 1))
     ans = []
-    while q:
-        for _ in range(y - 1):
-            q.append(q.popleft())
-        ans.append(q.popleft())
+    for i in range(x):
+        i = i * 2
+        a, b = input_list[i]
+        i_list = [0] * a
+        i_list[b] = 1
+        q = deque(input_list[i + 1])
+        qi = deque(i_list)
+        is_answer = False
+        count = 0
+        while not is_answer:
+            large = max(q)
+            loop_a = q.popleft()
+            loop_b = qi.popleft()
+            if loop_b == 1 and large == loop_a:
+                count += 1
+                is_answer = True
+            else:
+                if loop_a == large:
+                    count += 1
+                else:
+                    q.append(loop_a)
+                    qi.append(loop_b)
+        ans.append(count)
     return ans
     return x, y, input_list
 
 
 """N, M Input"""
 # 숫자 n을 받은 경우
-# n = int(sys.stdin.readline())
+n = int(sys.stdin.readline())
 # m = int(sys.stdin.readline())
 
 # 숫자 n, m을 받은 경우
-n, m = map(int, sys.stdin.readline().split())
+# n, m = map(int, sys.stdin.readline().split())
 
 """List Input"""
 # string을 n줄 받는 경우
@@ -40,8 +58,8 @@ n, m = map(int, sys.stdin.readline().split())
 #     arr.append(int(sys.stdin.readline().replace('\n', '')))
 
 # list<int>를 n줄 받은 경우
-# for _ in range(n):
-#     arr.append(list(map(int,sys.stdin.readline().split())))
+for _ in range(2 * n):
+    arr.append(list(map(int, sys.stdin.readline().split())))
 
 # list<str>를 n줄 받은 경우
 # for _ in range(n):
@@ -62,6 +80,6 @@ n, m = map(int, sys.stdin.readline().split())
 #     arr.append(data)
 
 result = answer(n, m, arr)
-print(str(result).replace("[", "<").replace("]", ">"))
-# for i in result:
-#     print(i)
+# print(result)
+for i in result:
+    print(i)
