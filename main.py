44375d8ebcfec80dc35eef8d76aebf10f9c8d6a9
LambFerret <dignrjrl12@gmail.com>
2022-07-01 12:52:54 +0900

    5430 AC

--- main.py
+++ main.py
@@ -6,46 +6,53 @@ n, m, k, arr = 0, 0, 0, []
 
 
 # constant
-class RoundDeque:
+class AC:
     def __init__(self, initq):
         self.q = deque(initq)
-        self.count = 0
+        self.is_reverse = False
 
-    def pop(self):
-        return self.q.popleft()
+    def D(self):
+        if not self.is_reverse:
+            self.q.popleft()
+        else:
+            self.q.pop()
 
-    def move_left(self):
-        self.count += 1
-        self.q.append(self.pop())
+    def R(self):
+        self.is_reverse = not self.is_reverse
 
-    def cursor(self):
-        return self.q[0]
+
+def makeAC(ls: list):
+    try:
+        ac = AC(ls[2])
+        for i in ls[0]:
+            if i == 'D':
+                ac.D()
+            elif i == 'R':
+                ac.R()
+        a = list(map(int, ac.q))
+        if ac.is_reverse:
+            a.reverse()
+        return str(a).replace(' ','')
+    except IndexError:
+        return 'error'
 
 
 def answer(x, y, input_list):
-    q = RoundDeque(range(1, x + 1))
-    count = 0
-    for target_number in input_list:
-        while target_number != q.cursor():
-            q.move_left()
-        count += min(q.count, len(q.q)-q.count)
-        q.pop()
-        q.count = 0
+    ans = []
+    for i in input_list:
+        ans.append(makeAC(i))
 
-
-
-
-    return count
+    return ans
     return x, y, input_list
 
 
 """N, M Input"""
 # 숫자 n을 받은 경우
-# n = int(sys.stdin.readline())
+n = int(sys.stdin.readline())
 # m = int(sys.stdin.readline())
 
 # 숫자 n, m을 받은 경우
-n, m = map(int, sys.stdin.readline().split())
+# n, m = map(int, sys.stdin.readline().split())
 
 """List Input"""
 # string을 n줄 받는 경우
@@ -54,7 +61,7 @@ n, m = map(int, sys.stdin.readline().split())
 
 
 # int를 한줄에 받은 경우
-arr = list(map(int, sys.stdin.readline().split()))
+# arr = list(map(int, sys.stdin.readline().split()))
 
 # int를 n줄 받은 경우
 # for _ in range(n):
@@ -67,6 +74,20 @@ arr = list(map(int, sys.stdin.readline().split()))
 # list<str>를 n줄 받은 경우
 # for _ in range(n):
 #     arr.append(list(map(str, sys.stdin.readline().split())))
+
+# 번외 : list<str>를 3n줄 신기하게 받은 경우
+for _ in range(n):
+    # a = sys.stdin.readline().split()
+    temp = [
+        sys.stdin.readline().replace('\n', ''), int(sys.stdin.readline())
+    ]
+    if temp[1] == 0:
+        sys.stdin.readline()
+        temp.append([])
+    else:
+        temp.append(sys.stdin.readline().replace('\n', '').replace('[', '').replace(']', '').split(','))
+    arr.append(temp)
+    # arr.append(list(map(str, sys.stdin.readline().split())))
 
 # list<int>를 특정 문자열 까지 받는 경우
 # while True:
@@ -83,6 +104,6 @@ arr = list(map(int, sys.stdin.readline().split()))
 #     arr.append(data)
 
 result = answer(n, m, arr)
-print(result)
-# for i in result:
-#     print(i)
+# print(result)
+for i in result:
+    print(i)
