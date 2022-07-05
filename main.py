99dcc3814065bcbed573aa7d2385e76920e995cf
LambFerret <dignrjrl12@gmail.com>
2022-07-05 14:32:33 +0900

    2630 색종이 만들기

--- main.py
+++ main.py
@@ -1,49 +1,32 @@
 import sys
-from collections import deque
 
 # init
 n, m, k, arr = 0, 0, 0, []
 
-
+w = 0
+b = 0
 # constant
-class AC:
-    def __init__(self, initq):
-        self.q = deque(initq)
-        self.is_reverse = False
-
-    def D(self):
-        if not self.is_reverse:
-            self.q.popleft()
-        else:
-            self.q.pop()
-
-    def R(self):
-        self.is_reverse = not self.is_reverse
-
-
-def makeAC(ls: list):
-    try:
-        ac = AC(ls[2])
-        for i in ls[0]:
-            if i == 'D':
-                ac.D()
-            elif i == 'R':
-                ac.R()
-        a = list(map(int, ac.q))
-        if ac.is_reverse:
-            a.reverse()
-        return str(a).replace(' ','')
-    except IndexError:
-        return 'error'
-
-
 def answer(x, y, input_list):
-    ans = []
-    for i in input_list:
-        ans.append(makeAC(i))
+    recursive(0, 0, len(input_list))
 
-    return ans
+    return w, b
     return x, y, input_list
+
+def recursive(x1, y1, length):
+    global w, b, arr
+    unit = arr[x1][y1]
+    for i in range(x1, x1+length):
+        for j in range(y1, y1+length):
+            if unit != arr[i][j]:
+                recursive(x1, y1, length // 2)
+                recursive(x1 + length // 2, y1, length // 2)
+                recursive(x1, y1 + length // 2, length // 2)
+                recursive(x1 + length // 2, y1 + length // 2, length // 2)
+                return;
+    if unit == 0:
+        w += 1
+    else:
+        b += 1
 
 
 """N, M Input"""
@@ -68,26 +51,12 @@ n = int(sys.stdin.readline())
 #     arr.append(int(sys.stdin.readline().replace('\n', '')))
 
 # list<int>를 n줄 받은 경우
-# for _ in range(n):
-#     arr.append(list(map(int,sys.stdin.readline().split())))
+for _ in range(n):
+    arr.append(list(map(int,sys.stdin.readline().split())))
 
 # list<str>를 n줄 받은 경우
 # for _ in range(n):
 #     arr.append(list(map(str, sys.stdin.readline().split())))
-
-# 번외 : list<str>를 3n줄 신기하게 받은 경우
-for _ in range(n):
-    # a = sys.stdin.readline().split()
-    temp = [
-        sys.stdin.readline().replace('\n', ''), int(sys.stdin.readline())
-    ]
-    if temp[1] == 0:
-        sys.stdin.readline()
-        temp.append([])
-    else:
-        temp.append(sys.stdin.readline().replace('\n', '').replace('[', '').replace(']', '').split(','))
-    arr.append(temp)
-    # arr.append(list(map(str, sys.stdin.readline().split())))
 
 # list<int>를 특정 문자열 까지 받는 경우
 # while True:
